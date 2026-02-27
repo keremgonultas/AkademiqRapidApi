7e19bb26c2bd65a3d70d7aaa2acc21b5fe807c1f
Kerem Gönültaş <keremgonultas@ogr.eskisehir.edu.tr>
2026-02-27 22:23:18 +0300

    Hava Durumu + Döviz Kuru

---
+++ AkademiqRapidApi/Controllers/WeatherController.cs
@@ -0,0 +1,35 @@
+﻿using AkademiqRapidApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace AkademiqRapidApi.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public IActionResult Index( )
+        {
+
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://open-weather13.p.rapidapi.com/city?city=istanbul&lang=TR"),
+                Headers =
+    {
+        { "x-rapidapi-key", "fab1dc0c34msha15b0520af426b0p15289fjsndb95a722c59f" },
+        { "x-rapidapi-host", "open-weather13.p.rapidapi.com" },
+    },
+            };
+           
+            var response = client.SendAsync(request).Result;
+
+            var jsonBody = response.Content.ReadAsStringAsync().Result;
+
+            var values = JsonSerializer.Deserialize<WeatherViewModel.Rootobject>(jsonBody);
+            
+            return View(values);
+        }
+    }
+}
---
+++ AkademiqRapidApi/Models/ConvertCurrencyViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace AkademiqRapidApi.Models
+{
+    public class ConvertCurrencyViewModel
+    {
+
+        public class Rootobject
+        {
+            public string from { get; set; }
+            public To to { get; set; }
+        }
+
+        public class To
+        {
+            public float TRY { get; set; }
+        }
+
+
+
+    }
+}
